1d68105853ecac4025f92b93fc50784874838553
Christoffer Fink <christofferfink@gmail.com>
2013-08-02 22:49:50 +0200

    Added DocTest project
    Added tests for:
    - Design
    - DesignSpace

---
+++ Java/src/DocTest/src/se/miun/itm/input/model/design/DesignSpaceTest.java
@@ -0,0 +1,193 @@
+/*-- $Copyright (C) 2012-13 Felix Dobslaw$
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package se.miun.itm.input.model.design;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import se.miun.itm.input.model.InPUTException;
+
+/**
+ * The following tests are explicitly meant as documentation tests.
+ * AKA executable documentation. It is not necessarily the case that
+ * the tested behavior is the right behavior, in the sense that this
+ * is what the program is supposed to do. These tests are intended to
+ * document what the program in fact does, given some corner cases.
+ * 
+ * See the configuration files for more detailed comments.
+ * 
+ * @author Christoffer Fink
+ */
+public class DesignSpaceTest {
+	/**
+	 * This test fails to create a DesignSpace from the configuration
+	 * due to a circular dependency. The name of the first parameter
+	 * is parsed as a literal value rather than a reference to a param.
+	 * This results in a NumberFormatException.
+	 * @throws InPUTException never
+	 */
+	@Test(expected=NumberFormatException.class)
+	public void shortCircularDependencyCausesException()
+			throws InPUTException {
+		final String designSpaceFile = "circularDependencySpace01.xml";
+		new DesignSpace(designSpaceFile);
+	}
+
+	/**
+	 * This test is similar to the shortCircularDependency test. The only
+	 * difference is that the chain consists of three parameters rather
+	 * than two. Creating the DesignSpace works fine in this case, but
+	 * using the space to create a Design does not work.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void longerCircularDependencyCausesDifferentException()
+			throws InPUTException {
+		final String designSpaceFile = "circularDependencySpace02.xml";
+		DesignSpace space = new DesignSpace(designSpaceFile);
+		try {
+			space.nextDesign("design");
+			fail("Trying to create the Design was supposed to fail.");
+		} catch(InPUTException e) { }
+	}
+
+	/**
+	 * While the short and long circularDependency tests only used exclMin
+	 * as a relation, this circular dependency mixes all four kinds. The
+	 * chain is therefore four parameters long. The result is the same as
+	 * for the longerCircularDependency test.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void mixedCircularDependencyCausesException()
+			throws InPUTException {
+		final String designSpaceFile = "circularDependencySpace03.xml";
+		DesignSpace space = new DesignSpace(designSpaceFile);
+		try {
+			space.nextDesign("design");
+			fail("Trying to create the Design was supposed to fail.");
+		} catch(InPUTException e) { }
+	}
+
+	/**
+	 * This test combines 02 and 03. In both tests, creating a DesignSpace
+	 * works. The tests only fail when trying to create a Design. However,
+	 * including both groups of parameters prevents even the DesignSpace
+	 * from being created.
+	 * @throws InPUTException never
+	 */
+	@Test(expected=StackOverflowError.class)
+	public void twoUnrelatedGroupsOfParametersCauseError()
+			throws InPUTException {
+		final String designSpaceFile = "circularDependencySpace04.xml";
+		new DesignSpace(designSpaceFile);
+	}
+
+	/**
+	 * This test shows that even a fixed parameter will be null in an empty
+	 * Design.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void fixedValueInEmptyDesignIsAlsoNull() throws InPUTException {
+		final String designSpaceFile = "testSpace.xml";
+		DesignSpace space = new DesignSpace(designSpaceFile);
+
+		// When generating a normal, non-empty Design, F should be 43.
+		IDesign nonEmpty = space.nextDesign("nonempty");
+		final int f = nonEmpty.getValue("F");
+		assertEquals("F is supposed to be fixed to 43", 43, f);
+
+		// However, when generating an empty design, even F is null.
+		IDesign empty = space.nextEmptyDesign("empty");
+		assertNull("F must be null in an empty Design.", empty.getValue("F"));
+
+		// Confirm that F is indeed fixed.
+		try {
+			empty.setValue("F", 2);
+			fail("Setting F to anything but the fixed value should fail.");
+		} catch(InPUTException e) { }
+	}
+
+	/**
+	 * This test confirms that it is impossible to create a DesignSpace
+	 * where a parameter has explicit overlapping restrictions (the min/max
+	 * limits leave no valid values).
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void creatingImpossibleDesignSpaceFails() throws InPUTException {
+		// All of these should fail when trying to create the DesignSpace.
+		final String[] designSpaceFiles = {
+			"impossibleSpace01.xml", "impossibleSpace02.xml",
+			"impossibleSpace03.xml",
+		};
+		for(String file : designSpaceFiles) {
+			try {
+				new DesignSpace(file);
+				fail(file + " should cause an exception.");
+			} catch(InPUTException e) { }
+		}
+	}
+
+	/**
+	 * This test demonstrates a bug.
+	 * The DesignSpace defines A such that the only valid value is 2.
+	 * B is then defined to be at least 2, but strictly less than A, which
+	 * is impossible. Not only can a DesignSpace be created from this
+	 * configuration, a Design can also be created based on the DesignSpace.
+	 * A is initialized as expected, but B is initialized to an illegal value.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void creatingDesignFromImpossibleDesignSpaceUnexpectedlyWorks()
+			throws InPUTException {
+		final String designSpaceFile = "impossibleSpace04.xml";
+		DesignSpace space = new DesignSpace(designSpaceFile);
+		// Because the set of possible values for B is empty, one would
+		// expect that a design cannot be created.
+		IDesign design = space.nextDesign("impossibleDesign");
+
+		// B is explicitly defined not to be able to take on the same
+		// value as A, yet the two are equal.
+		final int b = design.getValue("B");
+		final int a = design.getValue("A");
+		assertEquals("A and B can only be equal.", a, b);
+		// Since B could only be set to an invalid value, trying to set
+		// it to the same value again fails as expected.
+		try {
+			design.setValue("B", b);
+			fail("Cannot re-set B to the same value.");
+		} catch(IllegalArgumentException e) { }
+	}
+
+	/**
+	 * This test confirms that inclMin=x and inclMax=x is a valid range.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void creatingPossibleDesignSpace() throws InPUTException {
+		final String designSpaceFile = "possibleSpace.xml";
+		new DesignSpace(designSpaceFile);
+	}
+}
---
+++ Java/src/DocTest/src/se/miun/itm/input/model/design/DesignTest.java
@@ -0,0 +1,158 @@
+/*-- $Copyright (C) 2012-13 Felix Dobslaw$
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package se.miun.itm.input.model.design;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import se.miun.itm.input.model.InPUTException;
+
+/**
+ * The following tests are explicitly meant as documentation tests.
+ * AKA executable documentation. It is not necessarily the case that
+ * the tested behavior is the right behavior, in the sense that this
+ * is what the program is supposed to do. These tests are intended to
+ * document what the program in fact does, given some corner cases.
+ * 
+ * See the configuration files for more detailed comments.
+ * 
+ * @author Christoffer Fink
+ */
+public class DesignTest {
+	/**
+	 * This test arguably demonstrates a bug.
+	 * A is defined as being strictly bigger than B, yet A and B have the
+	 * same value in the Design that is being imported.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void importingDesignWithOutOfRangeValueSucceeds()
+			throws InPUTException {
+		final String designFile = "outOfRangeDesign01.xml";
+		new Design(designFile);
+	}
+
+	/**
+	 * This test arguably demonstrates a bug.
+	 * Setting A without first touching B results in an InPUTException
+	 * because the expression 'B' cannot be evaluated.
+	 * Since all values are within their defined ranges, it seems that
+	 * setting A should succeed.
+	 * 
+	 * @see #settingDependentValueSucceedsOnlyIfFirstTouchingDependee
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void settingDependentValueFailsUnlessFirstTouchingDependee()
+			throws InPUTException {
+		final String designFile = "outOfRangeDesign02.xml";
+		Design d = new Design(designFile);
+		try {
+			d.setValue("A", 6);
+			fail("Setting A without first setting B is expected to fail.");
+		} catch(InPUTException e) { }
+	}
+	
+	/**
+	 * This test is almost exactly the same as the fails
+	 * test above. The only difference is that the B parameter is first
+	 * accessed. (It does not matter whether it is set or only retrieved,
+	 * touching it in any way solves the problem.)
+	 * 
+	 * @see #settingDependentValueFailsUnlessFirstTouchingDependee
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void settingDependentValueSucceedsOnlyIfFirstTouchingDependee()
+			throws InPUTException {
+		final String designFile = "outOfRangeDesign02.xml";
+		Design d = new Design(designFile);
+		// Get the imported value and discard it.
+		d.getValue("B");
+		// Now A can be set without problems.
+		d.setValue("A", 6);
+	}
+
+	/**
+	 * This test shows that setting the value of a parameter that some other
+	 * parameter depends on (dependee) does not affect the dependent parameter.
+	 * Technically, setting the dependee to an inappropriate value (as is done
+	 * in this test) places the dependent parameter out of range, thereby
+	 * invalidating the Design.
+	 * @throws InPUTException never
+	 */
+	@Test
+	public void settingDependeeValueDoesNotAffectDependent()
+			throws InPUTException {
+		final String designFile = "outOfRangeDesign02.xml";
+		Design d = new Design(designFile);
+		d.setValue("B", 5);
+		final int a = d.getValue("A");
+		assertEquals("A should be 5 in the imported Design.", 5, a);
+		// This is exactly what we would expect.
+		// However, A is already out of range before trying to set it.
+		try {
+			d.setValue("A", a);
+			fail("A is now out of range. Cannot re-set the original value.");
+		} catch(IllegalArgumentException e) { }
+	}
+
+	/**
+	 * This test arguably demonstrates a bug.
+	 * By extending the scope of a design, the extending design can have its
+	 * parameters set even if the design has been set to read-only.
+	 * While the local parameters take precedent, which means that only
+	 * parameters that only exist in the extending scope can be set, any
+	 * parameter can still be set, simply by extending an empty Design so
+	 * that all of the parameters are unique.
+	 * @throws InPUTException
+	 */
+	@Test
+	public void extendingScopeCanCircumventReadOnly() throws InPUTException {
+		final String designFile = "outOfRangeDesign02.xml";
+		final String extendingDesignFile = "extendingDesign.xml";
+		Design design = new Design(designFile);
+		Design extendingDesign = new Design(extendingDesignFile);
+		// Setting both Designs to read-only.
+		design.setReadOnly();			// This isn't really relevant.
+		extendingDesign.setReadOnly();
+		
+		final int origZ = extendingDesign.getValue("Z");
+		
+		// Trying to set a value should now fail.
+		try {
+			extendingDesign.setValue("Z", 1);
+			fail("Cannot set any parameters on a read-only Design.");
+		} catch(InPUTException e) { }
+		
+		// However, now the extendingDesign is used to extend design.
+		design.extendScope(extendingDesign);
+		// Not only can setValue be called on design (which is read-only),
+		// but the extendingDesign (which is also read-only) will have its
+		// parameter updated.
+		design.setValue("Z", 1);
+		
+		final int z = extendingDesign.getValue("Z");
+		assertTrue("The read-only design should be changed.", origZ != z);
+	}
+}
